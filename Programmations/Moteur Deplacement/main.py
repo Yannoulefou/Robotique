4140ea913ea1db578913cf54904a42a03617c51d
tttienthinh <tranthuongtienthinh@gmail.com>
2024-02-05 16:37:51 +0100

    deplacement ok

--- Programmations/Moteur Deplacement/main.py
+++ Programmations/Moteur Deplacement/main.py
@@ -1,4 +1,5 @@
 from BaseDiff import BaseDiff, BaseDiffCalcul
+import time
 
 # Simulation ou connecté à une Arduino
 is_simulation = False
@@ -15,10 +16,11 @@ else:
     base_diff = BaseDiff(board=board, x=225, y=225, angle=0, pinsG=[4, 5, 6, 7], pinsD=[8, 9, 10, 11]   )
 
 # Ceci est un exemple de liste d'actions à effectuer pour le robot
+"""
 liste_actions = [ # Liste des actions à effectuer dans un couple (fonctions, arguments)
-    (base_diff.move, {'vitesseG': 700, 'vitesseD': 700, 'pasG': 3525, 'pasD': 3525}), # Avancer tout droit (env. 1096mm diviser par 0.31)
+    (base_diff.move, {'vitesseG': 700, 'vitesseD': 700, 'pasG': 850, 'pasD': 850}), # Avancer tout droit (env. 1096mm diviser par 0.31)
     (base_diff.move, {'vitesseG': 700, 'vitesseD': 700, 'pasG': 0, 'pasD': 281}), # Tourner à gauche à 145 °
-    (base_diff.move, {'vitesseG': 700, 'vitesseD': 700, 'pasG': 2450, 'pasD': 2450 }), # Avancer tout droit
+    (base_diff.move, {'vitesseG': 700, 'vitesseD': 700, 'pasG': 850, 'pasD': 850 }), # Avancer tout droit
     (base_diff.move, {'vitesseG': 700, 'vitesseD': 700, 'pasG': 0, 'pasD': 175 }), #Tourner à gauche à 90 °
     (base_diff.move, {'vitesseG': 700, 'vitesseD': 700, 'pasG': 2430, 'pasD': 2430 }), # Avancer tout droit
     (base_diff.move, {'vitesseG': 700, 'vitesseD': 700, 'pasG': 0, 'pasD': 281 }), # Tourner à gauche à 145 °
@@ -34,10 +36,27 @@ liste_actions = [ # Liste des actions à effectuer dans un couple (fonctions, ar
     (base_diff.move, {'vitesseG': 700, 'vitesseD': 700, 'pasG': 0, 'pasD': 88 }), # Tourner à gauche à 45 °
     (base_diff.move, {'vitesseG': 700, 'vitesseD': 700, 'pasG': 707, 'pasD': 707 }) # Avancer tout droit
 ]
+"""
+# Ceci est un exemple de liste d'actions à effectuer pour le robot
+liste_actions = [ # Liste des actions à effectuer dans un couple (fonctions, arguments)
+    (base_diff.move, {'vitesseG': 400, 'vitesseD': 400, 'pasG': 200, 'pasD': 0 }), # 45 °
+    (base_diff.move, {'vitesseG': 400, 'vitesseD': 400, 'pasG': 700, 'pasD': 700}), # Avancer tout droit (env. 1096mm diviser par 0.31)
+    (base_diff.move, {'vitesseG': 200, 'vitesseD': 200, 'pasG': -550, 'pasD': 550}), # Demi tour
+    (base_diff.move, {'vitesseG': 400, 'vitesseD': 400, 'pasG': 500, 'pasD': 500 }), # Avancer tout droit
+    (base_diff.move, {'vitesseG': 400, 'vitesseD': 400, 'pasG': -600, 'pasD': -600 }), # Reculer tout droit
+     (base_diff.move, {'vitesseG': 400, 'vitesseD': 400, 'pasG': -400, 'pasD': 400}), # Tourner 
+    (base_diff.move, {'vitesseG': 400, 'vitesseD': 400, 'pasG': 600, 'pasD': 600 }), # Avancer tout droit
+     (base_diff.move, {'vitesseG': 400, 'vitesseD': 400, 'pasG': 250, 'pasD': -250}), # Tourner 
+    (base_diff.move, {'vitesseG': 400, 'vitesseD': 400, 'pasG': 600, 'pasD': 600 }), # Avancer tout droit
+    (base_diff.move, {'vitesseG': 400, 'vitesseD': 400, 'pasG': -1200, 'pasD': -1200 }), # Reculer tout droit
+    (base_diff.move, {'vitesseG': 200, 'vitesseD': 200, 'pasG': -550, 'pasD': 550}), # Demi tour
+    (base_diff.move, {'vitesseG': 400, 'vitesseD': 400, 'pasG': 700, 'pasD': 700 }), # Avancer tout droit
+]
 
 # Réalisation des actions
 for fonction, kwargs in liste_actions:
     fonction(**kwargs) # On appelle la fonction avec les arguments
+    time.sleep(0.5) # On attend 1 seconde
     print(f"Il a pour position {base_diff.x}, {base_diff.y} et pour angle {base_diff.angle}.")
 
 # Résultats
