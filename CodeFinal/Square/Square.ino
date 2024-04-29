4566d5373f315a7623c11fca5248297c9d7489bd
Fanny46 <fanny.daubet@gmail.com>
2024-04-29 15:12:25 +0200

    gyroscope et millis pour rotation

--- CodeFinal/Square/Square.ino
+++ CodeFinal/Square/Square.ino
@@ -1,4 +1,7 @@
 #include <AccelStepper.h>
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 #include <math.h> // pour utiliser PI
 // AccelStepper https://www.youtube.com/watch?v=QRCvC5xhJCw
 // Pour éviter la surchauffe des moteurs Vref https://www.youtube.com/watch?v=BV-ouxhZamI
@@ -24,6 +27,17 @@ const int enPin = 8; // enable Pin pour allumer le shield
 AccelStepper stepper_L(1, 2, 5);  // 1=using_controller, pull, dir
 AccelStepper stepper_R(1, 3, 6);
 
+// Pour le gyroscope
+
+# define DT 0.1
+unsigned long previousMillis = 0; // Temps du dernier update rotation
+float mean_err_gz = 0.0;
+
+// Initialisation de l'objet MPU6050
+Adafruit_MPU6050 mpu;
+
+
+
 void setup() {
  	Serial.begin(9600);
   // Allumer le shield
@@ -35,6 +49,27 @@ void setup() {
   // setup Right
   stepper_R.setMaxSpeed(100.0);
   stepper_R.setAcceleration(50.0);
+
+  // Initalize serial monitor
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10);
+  }
+
+  // Try to initialize gyroscope
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.println("");
+  delay(100);
+
+  Serial.println("Gyro initialisé");
 }
 
 void loop() {
@@ -47,17 +82,26 @@ void loop() {
     line = false;
 
     } else {
+    compute_error_gyro(); // peut-être à mettre dans la void setup
     turn(PI/2);
     line = true;
 
     }
 
   }
-  
+
+  unsigned long currentMillis = millis();
+
   // Run motor
   stepper_L.run();
   stepper_R.run();
-
+  if (!line) {
+    float delay = currentMillis - previousMillis
+    // Exécute la tâche de rotation toutes les DT millisecondes
+    if (delay >= DT) {
+      compute_angle_gyro(delay);
+      previousMillis = currentMillis;
+  }
 }
 
 
@@ -73,6 +117,39 @@ void turn(float angle) {
   // Move both stepper motors simultaneously
     stepper_L.moveTo(stepper_L.currentPosition() + steps);
     stepper_R.moveTo(stepper_R.currentPosition() - steps);
+}
+
+void compute_error_gyro(){
+  float sum_err_gz = 0.0;
+  const int n = 1000;
+  for (int i = 0; i < n; i++) {
+    // Lecture des données du gyroscope
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    // Calcul de la vitesse de rotation (en radian/s)
+    float err_gz = g.gyro.z;
+    sum_err_gz += err_gz;
+    delay(1);
+  }
+  // Calcul de la moyenne
+  mean_err_gz = sum_err_gz / n;
+}
+
+
+void compute_angle_gyro(delay) {
+  float angle_gyro = 0.0;
+  // Lecture des données du gyroscope
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  // Lecture des données de rotation (en rad/s)
+  float gz = g.gyro.z;
+  angle_gyro += (gz-mean_err_gz) * (delay/1000); // Mise à jour de la rotation
+
+  // Affichage de la rotation
+  Serial.print(angle_gyro); // Affichage de la rotation
+  Serial.print(" ; ");
+  Serial.println(angle_gyro * 180 / M_PI); // Affichage de la rotation en degrés
 }
 
 
